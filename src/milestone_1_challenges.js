178b13739ce23b19a2e9072111586245110811ff
de-scientist <gitaumark502@gmail.com>
2025-10-02 19:33:21 +0300

    Challenge 2. Convert Minutes into seconds

--- src/milestone_1_challenges.js
+++ src/milestone_1_challenges.js
@@ -1,7 +1,21 @@
-//1. Return the Sum of Two Numbers
+//Challenge 1. Return the Sum of Two Numbers
 function addition (num1, num2) {
     return num1 + num2;
 }
-let sum = addition(15, 27);
-console.log(sum);
+console.log(addition(3,5)); // returns 8
+console.log(addition(-6, 9)); // returns 3
+
+
+
+//Challenge 2. Convert Minutes into seconds
+function convert(minutes) {
+    return minutes * 60;
+}
+console.log(convert(5)); // returns 300
+console.log(convert(2)); // returns 120
+
+
+
+
+
 
